c10c7240d229f52cf4b60ce37bc18f0ede9477cb
Hoarau Matthias <h.matthias@live.fr>
2022-11-25 14:18:13 +0400

    #3 Add validation and error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -24,6 +24,35 @@ const terms = document.querySelector('#checkbox1')
 const submitForm = document.querySelector('.btn-submit')
 const form = document.querySelector('form')
 
+// Event
+first.addEventListener('blur', () =>
+	validInputElement(first, checkFirst(first.value), errorElement.first)
+)
+first.addEventListener('input', () =>
+	validInputElement(first, checkFirst(first.value), errorElement.first)
+)
+last.addEventListener('blur', () =>
+	validInputElement(last, checkLast(last.value), errorElement.last)
+)
+last.addEventListener('input', () =>
+	validInputElement(last, checkLast(last.value), errorElement.last)
+)
+email.addEventListener('blur', () =>
+	validInputElement(email, checkEmail(email.value), errorElement.email)
+)
+email.addEventListener('input', () =>
+	validInputElement(email, 	checkEmail(email.value), errorElement.email)
+)
+quantity.addEventListener('blur', () =>
+	validInputElement(quantity, checkNumber(quantity.value), errorElement.quantity)
+)
+quantity.addEventListener('input', () =>
+	validInputElement(quantity, checkNumber(quantity.value), errorElement.quantity)
+)
+terms.addEventListener('change', () =>
+	validInputElement(terms, checkTerms(terms.checked), errorElement.terms)
+)
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal))
 modalClose.addEventListener('click', closeModal)
@@ -34,6 +63,17 @@ function launchModal() {
 // Close Modal form
 function closeModal() {
 	modalbg.style.display = 'none'
+}
+
+// variable object error
+const errorElement = {
+	first: 'Veuillez entrer 2 caractères ou plus pour le champ du prenom.',
+	last: 'Veuillez entrer 2 caractères ou plus pour le champ du nom.',
+	email: 'Veuillez entrer une adresse emaail valide "exemple@exemple.fr".',
+	birthdate: 'Vous devez entrer votre date de naissance et avoir 18 ans.',
+	quantity: 'Veuillez entrer un nombre valide positif.',
+	city: 'Vous devez choisir une option.',
+	terms: 'Vous devez vérifier que vous acceptez les termes et conditions.',
 }
 
 //Prevent send form
@@ -128,13 +168,12 @@ function checkBirthdate(birthdate) {
  * @return {Boolean} if city is checked
  */
 function checkCity() {
-	let checked = false
 	cities.forEach((city) => {
 		if (city.checked) {
-			checked = true
+			return true
 		}
 	})
-	return checked
+	return false
 }
 
 /**
@@ -144,4 +183,65 @@ function checkCity() {
  */
 function checkTerms(term) {
 	return term
+}
+
+/**
+ * Apply error On DOM
+ *
+ * @param {Element} element
+ * @param {String} textError
+ */
+function applyErrorViewInput(element, textError) {
+	element.parentNode.setAttribute('data-error', textError)
+	element.parentNode.setAttribute('data-error-visible', 'true')
+}
+
+/**
+ * Remove error from DOM
+ *
+ * @param {Element} element
+ */
+function removeErrorViewInput(element) {
+	element.parentNode.removeAttribute('data-error-visible')
+	element.parentNode.removeAttribute('data-error')
+}
+
+/**
+ * function validation form input eventlistener
+ *
+ * @param {Element} element
+ * @param {Callback} cl function callback
+ * @param {String} errorTxt in const errorElement
+ */
+function validInputElement(element, cl, errorTxt) {
+	if (cl) {
+		removeErrorViewInput(element)
+	} else {
+		applyErrorViewInput(element, errorTxt)
+	}
+}
+
+// validate the form
+function validate() {
+	formData.forEach((form) => {
+		form.removeAttribute('data-error')
+		form.removeAttribute('data-error-visible')
+	})
+	if (!checkFirst(first.value)) {
+		applyErrorViewInput(first, errorElement.first)
+	} else if (!checkLast(last.value)) {
+		applyErrorViewInput(last, errorElement.last)
+	} else if (!checkEmail(email.value)) {
+		applyErrorViewInput(email, errorElement.email)
+	} else if (!checkBirthdate(birthdate.value)) {
+		applyErrorViewInput(birthdate, errorElement.birthdate)
+	} else if (!checkNumber(quantity.value)) {
+		applyErrorViewInput(quantity, errorElement.quantity)
+	} else if (!checkCity()) {
+		applyErrorViewInput(cities[0], errorElement.city)
+	} else if (!checkTerms(terms.checked)) {
+		applyErrorViewInput(terms, errorElement.terms)
+	} else {
+		console.log('valider !!!! Bravo')
+	}
 }
